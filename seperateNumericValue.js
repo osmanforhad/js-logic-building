2209fd3dbc20a41bc948c71d263a29e6e5144598
osmanforhad <osmanforhad93@gmail.com>
2025-06-26 23:33:05 +0600

    solve the seperating issue of numeric and string value

--- seperateNumericValue.js
+++ seperateNumericValue.js
@@ -1,4 +1,21 @@
 /**
  * Seperate Numeric value from
- * string array
+ * a array
  */
+let arr = [1, 2, 3, "sorif", "fokhrul", 7, 8, "osman", 4, 5];
+let result = arr.filter((val) => {
+  return typeof val === "number";
+});
+console.log(result);
+//sorting the numeric value
+let sorting = result.sort((a, b) => a - b);
+console.log(sorting);
+
+/**
+ * Seperate String value from
+ * a array
+ */
+let stringValue = arr.filter((val) => {
+  return typeof val === "string";
+});
+console.log(stringValue);
