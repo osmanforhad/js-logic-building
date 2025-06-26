92146d91ceccc2685ae494ee9546d1774f0eee1e
osmanforhad <osmanforhad93@gmail.com>
2025-06-26 22:21:27 +0600

    sorting array alpahavetically like a to z

---
+++ sortingArray.js
@@ -0,0 +1,14 @@
+//How to sort an Array
+let arr = ["sorif", "remon", "osman", "arif", "tajul"];
+/**
+ * method one
+ *sort by
+ * a to z
+ */
+console.log(arr.sort());
+
+/**
+ * method two
+ *sort by
+ * numerically
+ */
