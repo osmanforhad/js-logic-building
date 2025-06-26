2ff048eba30903cb25ffecfac903f49081df32d2
osmanforhad <osmanforhad93@gmail.com>
2025-06-26 22:28:11 +0600

    solve the array sortung question

--- sortingArray.js
+++ sortingArray.js
@@ -1,14 +1,16 @@
 //How to sort an Array
-let arr = ["sorif", "remon", "osman", "arif", "tajul"];
 /**
  * method one
- *sort by
+ *sort by alphavetically
  * a to z
  */
-console.log(arr.sort());
+let arr_1 = ["sorif", "remon", "osman", "arif", "tajul"];
+console.log(arr_1.sort());
 
 /**
  * method two
  *sort by
  * numerically
  */
+let arr_2 = [2, 9, 5, 6, 3, 1, 4, 7, 8, 100, 115, 110];
+console.log(arr_2.sort((a, b) => a - b));
