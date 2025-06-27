e39740a8fa5e2555e563db03ee0ee4862e7d4bba
osmanforhad <osmanforhad93@gmail.com>
2025-06-27 16:54:41 +0600

    making flat array and get ther length issues solve

--- getArrayLengthAndMakeFlate.js
+++ getArrayLengthAndMakeFlate.js
@@ -3,4 +3,9 @@
  * and
  * make the array flate
  */
-let arr = [5, 7, [4, 2[4]]]; 
+let array_1 = [5, 7, [4, 2, [4]]];
+//making flate array
+let array_2 = array_1.flat(Infinity);
+console.log("There is a flate array: ", array_2);
+//get array length
+console.log("The Length of this array is: ", array_2.length); 
