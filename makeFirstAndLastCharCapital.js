4dcc2e1bd30f2bcfa33ed6d8c5715ce14a42679c
osmanforhad <osmanforhad93@gmail.com>
2025-06-27 17:14:31 +0600

    solve the issue

--- makeFirstAndLastCharCapital.js
+++ makeFirstAndLastCharCapital.js
@@ -3,3 +3,10 @@
  * capital of a string
  */
 let country = "bangladesh";
+//get first index of the string and make capital
+let cap_firstChar = country[0].toUpperCase();
+//get last index of the string and make capital
+let cap_lastChar = country[country.length - 1].toUpperCase();
+//cocate the total string make it as requiremetn
+let final_String = country.slice(1, 9);
+console.log(cap_firstChar + final_String + cap_lastChar);
