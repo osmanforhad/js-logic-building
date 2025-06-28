642f521c339d2ae6929df46f7f95f67c1398b50d
osmanforhad <osmanforhad93@gmail.com>
2025-06-28 21:27:36 +0600

    solve the question

--- getMinAndMaxNumber.js
+++ getMinAndMaxNumber.js
@@ -3,3 +3,22 @@
  * without min and max method
  */
 let arr = [4, 2, 10, 0, 1];
+//method for get max value
+let newArr = arr.reduce((prev, curval) => {
+    if (prev > curval) {
+        return prev;
+    } else {
+        return curval;
+    }
+});
+console.log(newArr);
+
+//method for get min value
+let newArrs = arr.reduce((prev, curval) => {
+    if (prev < curval) {
+        return prev;
+    } else {
+        return curval;
+    }
+});
+console.log(newArrs);
