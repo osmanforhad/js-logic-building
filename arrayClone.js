ccd2f4bf23992e8052c331af674a81fb58eb3e5a
osmanforhad <osmanforhad93@gmail.com>
2025-06-28 21:19:56 +0600

    solve the issue

--- arrayClone.js
+++ arrayClone.js
@@ -3,3 +3,15 @@
  * [1,2,3]
  * expected output = [1,2,3,[1,2,3]]
  */
+let arr = [1, 2, 3];
+//method one
+let result = [...arr, arr];
+console.log(result);
+
+//method 2
+let cloneArr = (ar) => {
+    let newArr = [...ar];
+    newArr.push(ar);
+    return newArr;
+}
+console.log(cloneArr(arr));
