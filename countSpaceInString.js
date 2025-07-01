d22f1b3a7ed07e0e8e4e9fd58285fee555d031dc
osmanforhad <osmanforhad93@gmail.com>
2025-07-01 18:13:19 +0600

    solve the issue

--- countSpaceInString.js
+++ countSpaceInString.js
@@ -2,3 +2,20 @@
  * Count Space in a String
  */
 let institute = "Institue of computer science & technology";
+
+//define spaceCount method
+spaceCount = (instituteName) => {
+    let count = 0;
+
+    for (let i = 0; i < instituteName.length; i++) {
+
+        if (instituteName[i].includes(" ")) {
+            count++;
+        }
+
+    }
+    return count;
+}
+//calling the method and pasing argument
+let result = spaceCount(institute);
+console.log(result);
