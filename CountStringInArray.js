996bc05342ec0e78793e15725d4510c66b8166d8
osmanforhad <osmanforhad93@gmail.com>
2025-07-02 16:38:13 +0600

    question setup

---
+++ CountStringInArray.js
@@ -0,0 +1,14 @@
+/**
+ * Count String in Array
+ * 
+ * or count a string how many times repeat in a array
+ */
+const arr = [
+    "hello",
+    "world",
+    "js.developer",
+    "js.developer",
+    "js.developer",
+    "hello",
+    "match"
+];
