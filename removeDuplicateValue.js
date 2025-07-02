6e8e81f2369017c21aeb7facae8eaf8a026b673f
osmanforhad <osmanforhad93@gmail.com>
2025-07-02 16:32:51 +0600

    solve the issue

--- removeDuplicateValue.js
+++ removeDuplicateValue.js
@@ -11,3 +11,38 @@ let names = [
     "fokhrul",
     "osman"
 ];
+
+/**
+ * Solution Method one
+ */
+let uniqueArr = [...new Set(names)];
+console.log(uniqueArr);
+
+/**
+ * solutin method two
+ * @param {*} array 
+ * @returns 
+ */
+
+//define method for geting new value as output
+const newValue = (array) => {
+
+    let newArr = [];
+
+    for (let index = 0; index < array.length; index++) {
+
+        const element = array[index];
+
+        if (newArr.indexOf(element) === -1) {
+
+            newArr.push(array[index]);
+        }
+
+    }
+    return newArr;
+}
+
+//calling method and passing argument
+let result = newValue(names);
+
+console.log(result);
