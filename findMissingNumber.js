743fef4b152b5d18876644978545c6f236f72641
osmanforhad <osmanforhad93@gmail.com>
2025-07-04 18:28:18 +0600

    solve the issue

--- findMissingNumber.js
+++ findMissingNumber.js
@@ -3,3 +3,24 @@
  * an array
  */
 const arr = [1, 2, 3, 5];
+
+const getMissingNumber = (arr) => {
+
+    let missig_number = [];
+    //get min and max value from given array
+    let min_number = Math.min(...arr);
+    let max_number = Math.max(...arr);
+
+    for (let i = min_number; i < max_number; i++) {
+
+        if (arr.indexOf(i) < 0) {
+            missig_number.push(i);
+        }
+
+    }
+
+    return missig_number;
+
+}
+const result = getMissingNumber(arr);
+console.log(result);
