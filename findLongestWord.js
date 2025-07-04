dec09c25a66394fb55729044a5f5886ba5191901
osmanforhad <osmanforhad93@gmail.com>
2025-07-04 18:53:26 +0600

    solve the issues

--- findLongestWord.js
+++ findLongestWord.js
@@ -3,3 +3,17 @@
  * fron an sentence
  */
 const str = "The quick brown fox jumps over the lazy dog";
+
+const finLongestWord = (sentence) => {
+
+    const words = sentence.split(' ');
+    let longestWord = '';
+
+    for (const word of words) {
+        word.length > longestWord.length ? longestWord = word : longestWord;
+    }
+
+    return longestWord;
+}
+const result = finLongestWord(str);
+console.log(result);
