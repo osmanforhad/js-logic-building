83cf376d6a9e53d008bf70800186a3f37a60212b
osmanforhad <osmanforhad93@gmail.com>
2025-07-04 18:31:21 +0600

    question setup

---
+++ sortingArrayDataInOrder.js
@@ -0,0 +1,9 @@
+/**
+ * Sorting array data in a 
+ * spacifice order or Group
+ */
+const users = [
+    { name: "sorif", age: 33 },
+    { name: "osman", age: 32 },
+    { name: "fokhrul", age: 34 },
+];
