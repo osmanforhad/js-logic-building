97a99983d2be18fb92757cafcaedd28476e3b2ac
osmanforhad <osmanforhad93@gmail.com>
2025-07-05 11:10:48 +0600

    question setup

--- checkPelindomString.js
+++ checkPelindomString.js
@@ -3,3 +3,11 @@
  * Pelindom or not
  */
 let input_str = 'level';
+
+const isPlain = (str) => {
+    const revers_str = str.split("").reverse().join("");
+    return revers_str === str;
+
+}
+const result = isPlain(input_str);
+console.log(result);
---
+++ findIntersction.js
@@ -0,0 +1,6 @@
+/**
+ * Find Common Element 
+ * From Two Or Multiple Array
+ */
+const num1 = [1, 2, 3, 4, 5];
+const num2 = [3, 4, 5, 6, 7];
