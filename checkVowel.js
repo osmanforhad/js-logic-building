423a1ba2ab04e5b2720102353e86ca0155b131c5
osmanforhad <osmanforhad93@gmail.com>
2025-07-05 11:29:37 +0600

    solve the question

--- checkVowel.js
+++ checkVowel.js
@@ -3,3 +3,14 @@
  * from an String
  */
 const str = "Check Vowel";
+
+const countVowels = (str) => {
+    const vowels = [
+        'a', 'e', 'i', 'o', 'u'
+    ];
+    return str.toLowerCase().split('').reduce((acc, char) => {
+        return vowels.includes(char) ? acc + 1 : acc;
+    }, 0);
+}
+const result = countVowels(str);
+console.log(result);
