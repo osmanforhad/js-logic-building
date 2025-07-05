b02f9d27efac8119b413caec1f4a2786658c9aeb
osmanforhad <osmanforhad93@gmail.com>
2025-07-05 10:26:57 +0600

    question setup

---
+++ multipleItemsToArray.js
@@ -0,0 +1,21 @@
+/**
+ * Convert Multiple intems into a
+ * Single Array
+ */
+const users = [
+    {
+        name: 'Rahul', books: [
+            "Against empathy", 'War'
+        ]
+    },
+    {
+        name: 'Max', books: [
+            "Romeo and Juliet", 'Romantic'
+        ]
+    },
+    {
+        name: 'jane', books: [
+            'Harry', 'Peace', 'Lord', 'Ring'
+        ]
+    },
+];
