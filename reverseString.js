8e5233656614dbc3c070f5871e7042809062f1e1
osmanforhad <osmanforhad93@gmail.com>
2025-07-05 18:50:20 +0600

    solve the question

--- reverseString.js
+++ reverseString.js
@@ -3,3 +3,15 @@
  * without reverse method
  */
 const str = "Allah is Almighty";
+const getReverseStr = (str) => {
+
+    str = str.split(" ");
+    let result = [];
+    for (let i = str.length; i >= 0; i--) {
+        result.push(str[i]);
+    }
+    return result.join(" ");
+
+}
+const ouptput = getReverseStr(str);
+console.log(ouptput);
