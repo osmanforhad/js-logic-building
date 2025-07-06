dca0a9d9d786f1c9c32500419eca9effd7020141
osmanforhad <osmanforhad93@gmail.com>
2025-07-06 09:30:38 +0600

    solve the question

--- CheckSubString.js
+++ CheckSubString.js
@@ -4,3 +4,20 @@
  */
 const str = "Chek the Substring";
 const substring = 'The';
+
+const isSubStringExist = (str, subStr) => {
+    str = str.split(" ");
+
+    for (let i = 0; i < str.length; i++) {
+        let element = str[i].toLowerCase();
+
+        if (element === subStr.toLowerCase()) {
+            return true;
+        }
+
+    }
+    return false;
+
+}
+const result = isSubStringExist(str, substring);
+console.log(result);
