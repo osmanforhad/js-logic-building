5629fa8eb8e7785f7e8d7e4b384279168d21d9a8
osmanforhad <osmanforhad93@gmail.com>
2025-07-06 09:44:42 +0600

    solve the question

--- convertUpperCaseEveryAraryElement.js
+++ convertUpperCaseEveryAraryElement.js
@@ -3,3 +3,10 @@
  * arry element
  */
 const day = ['sun', 'mon', 'tue'];
+const newArray = [];
+for (let me of day) {
+    me = me.charAt(0).toLocaleUpperCase() + me.substring(1);
+    newArray.push(me);
+
+}
+console.log(newArray);
